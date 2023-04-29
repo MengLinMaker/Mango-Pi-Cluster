9f27a8bfca3cbe7ec4d1202a3d7dda1e4c91fa16
MengLinMaker <menglinmaker@gmail.com>
2023-04-29 21:10:43 +1000

    Add batman-adv setup

--- network/setup.sh
+++ network/setup.sh
@@ -2,3 +2,16 @@
 
 # Enable ssh capability
 touch /boot/ssh
+
+# Setup batman-adv WiFi mesh
+sudo apt install -y batctl
+
+echo 'batman-adv' | sudo tee --append /etc/modules
+echo 'denyinterfaces wlan0' | sudo tee --append /etc/dhcpcd.conf
+
+# Execute start-batman-adv on boot
+cp start-batman-adv.sh /etc
+chmod +x /etc/start-batman-adv.sh
+sed -i '2 i /etc/start-batman-adv.sh &' /etc/rc.local
+
+cp wlan0 /etc/network/interfaces.d
---
+++ network/start-batman-adv.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# batman-adv interface to use
+sudo batctl if add wlan0
+sudo ifconfig bat0 mtu 1468
+
+# Tell batman-adv this is a gateway client
+sudo batctl gw_mode client
+
+# Activates batman-adv interfaces
+sudo ifconfig wlan0 up
